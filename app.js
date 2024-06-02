230fc1e8fd73f949f2e98fd01a14804d9740b854
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-02 20:12:02 +0700

    js lab 104

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// write code here
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+const summaryValue = Object.values(salaries).reduce(
+  (sum, value) => sum + value,
+  0
+);
+console.log(summaryValue);
+const summaryKey = Object.keys(salaries).reduce(
+  (sum, key) => sum + salaries[key],
+  0
+);
+console.log(summaryKey);
+const summaryEntrie = Object.entries(salaries).reduce(
+  (sum, [key, value]) => sum + value,
+  0
+);
+console.log(summaryEntrie);
